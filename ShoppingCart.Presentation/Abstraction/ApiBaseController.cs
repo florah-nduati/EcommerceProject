a587dfca4153965d865d76675ff7ca565fed30ba
florah nyokabi nduati <florah.nduati@griffinglobaltech.com>
2026-03-13 11:14:05 +0300

    Add project files.

---
+++ ShoppingCart.Presentation/Abstraction/ApiBaseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Presentation.Abstraction
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public abstract class ApiBaseController : ControllerBase
+
+    {
+        protected readonly ISender sender;
+        {
+            Sender = sender;
+        }
+   
+    }
+}
